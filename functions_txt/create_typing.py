fd5e7f94d87f87894bc43df58bd9d07ccb30d013
hurasum <53011033+hurasum@users.noreply.github.com>
2020-10-16 12:34:17 +0200

    update create_typing paths

--- functions_txt/create_typing.py
+++ functions_txt/create_typing.py
@@ -1,4 +1,5 @@
 
+PATH = "C:/repository/esp32_python/functions_txt/{}"
 
 def parseFunction(parse_function, filter_word, in_class):
     new_line = ""
@@ -36,168 +37,168 @@ def parseTextFile(path, filter_word, in_class):
     return file_str
 
 def display():
-    dpy = open("C:/repository/esptool-master/lol/display.py", "w")
+    dpy = open(PATH.format("display.py"), "w")
     dpy.write("\"\"\"\nDisplay typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class TFT:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/display_functions.txt"
+    dtxt = PATH.format("display_functions.txt")
     file_str = parseTextFile(dtxt, "tft", True)
     dpy.write(file_str)
     dpy.close()
 
 def machine():
-    dpy = open("C:/repository/esptool-master/lol/machine.py", "w")
+    dpy = open(PATH.format("machine.py"), "w")
     dpy.write("\"\"\"\nMachine typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
 
-    dtxt = "C:/repository/esptool-master/lol/machine_functions.txt"
+    dtxt = PATH.format("machine_functions.txt")
     file_str = parseTextFile(dtxt, "machine", False)
     dpy.write(file_str)
     dpy.close()
 
 def neopixel():
-    dpy = open("C:/repository/esptool-master/lol/neo_pixel.py", "w")
+    dpy = open(PATH.format("neo_pixel.py"), "w")
     dpy.write("\"\"\"\nNeo Pixel typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class NeoPixel:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/neopixel_functions.txt"
+    dtxt = PATH.format("neopixel_functions.txt")
     file_str = parseTextFile(dtxt, "np", True)
     dpy.write(file_str)
     dpy.close()
 
 def adc():
-    dpy = open("C:/repository/esptool-master/lol/adc.py", "w")
+    dpy = open(PATH.format("adc.py"), "w")
     dpy.write("\"\"\"\nADC typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class ADC:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/adc_functions.txt"
+    dtxt = PATH.format("adc_functions.txt")
     file_str = parseTextFile(dtxt, "adc", True)
     dpy.write(file_str)
     dpy.close()
 
 def dac():
-    dpy = open("C:/repository/esptool-master/lol/dac.py", "w")
+    dpy = open(PATH.format("dac.py"), "w")
     dpy.write("\"\"\"\nDAC typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class DAC:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/dac_functions.txt"
+    dtxt = PATH.format("dac_functions.txt")
     file_str = parseTextFile(dtxt, "dac", True)
     dpy.write(file_str)
     dpy.close()
 
 def pwm():
-    dpy = open("C:/repository/esptool-master/lol/pwm.py", "w")
+    dpy = open(PATH.format("pwm.py"), "w")
     dpy.write("\"\"\"\nPWM typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class PWM:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/pwm_functions.txt"
+    dtxt = PATH.format("pwm_functions.txt")
     file_str = parseTextFile(dtxt, "pwm", True)
     dpy.write(file_str)
     dpy.close()
 
 def ow():
-    dpy = open("C:/repository/esptool-master/lol/one_wire.py", "w")
+    dpy = open(PATH.format("one_wire.py"), "w")
     dpy.write("\"\"\"\nOne Wire typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class OW:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/ow_functions.txt"
+    dtxt = PATH.format("ow_functions.txt")
     file_str = parseTextFile(dtxt, "ow", True)
     dpy.write(file_str)
     dpy.close()
 
 def rtc():
-    dpy = open("C:/repository/esptool-master/lol/rtc.py", "w")
+    dpy = open(PATH.format("rtc.py"), "w")
     dpy.write("\"\"\"\nRTC typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class RTC:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/rtc_functions.txt"
+    dtxt = PATH.format("rtc_functions.txt")
     file_str = parseTextFile(dtxt, "rtc", True)
     dpy.write(file_str)
     dpy.close()
 
 def timer_f():
-    dpy = open("C:/repository/esptool-master/lol/timer.py", "w")
+    dpy = open(PATH.format("timer.py"), "w")
     dpy.write("\"\"\"\nTimer typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class TM:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/timer_functions.txt"
+    dtxt = PATH.format("timer_functions.txt")
     file_str = parseTextFile(dtxt, "tm", True)
     dpy.write(file_str)
     dpy.close()
 
 def uart():
-    dpy = open("C:/repository/esptool-master/lol/uart.py", "w")
+    dpy = open(PATH.format("uart.py"), "w")
     dpy.write("\"\"\"\nUART typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class UART:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/uart_functions.txt"
+    dtxt = PATH.format("uart_functions.txt")
     file_str = parseTextFile(dtxt, "uart", True)
     dpy.write(file_str)
     dpy.close()
 
 def i2c():
-    dpy = open("C:/repository/esptool-master/lol/i2c.py", "w")
+    dpy = open(PATH.format("i2c.py"), "w")
     dpy.write("\"\"\"\nI2C typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class I2C:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/i2c_functions.txt"
+    dtxt = PATH.format("i2c_functions.txt")
     file_str = parseTextFile(dtxt, "i2c", True)
     dpy.write(file_str)
     dpy.close()
 
 def spi():
-    dpy = open("C:/repository/esptool-master/lol/spi.py", "w")
+    dpy = open(PATH.format("spi.py"), "w")
     dpy.write("\"\"\"\nSPI typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class SPI:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/spi_functions.txt"
+    dtxt = PATH.format("spi_functions.txt")
     file_str = parseTextFile(dtxt, "spi", True)
     dpy.write(file_str)
     dpy.close()
 
 def pin():
-    dpy = open("C:/repository/esptool-master/lol/pin.py", "w")
+    dpy = open(PATH.format("pin.py"), "w")
     dpy.write("\"\"\"\nSPI typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class PIN:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/pin_functions.txt"
+    dtxt = PATH.format("pin_functions.txt")
     file_str = parseTextFile(dtxt, "pin", True)
     dpy.write(file_str)
     dpy.close()
 
 def ds():
-    dpy = open("C:/repository/esptool-master/lol/ds_one_wire.py", "w")
+    dpy = open(PATH.format("ds_one_wire.py"), "w")
     dpy.write("\"\"\"\nSPI typing class\n\"\"\"\n")
     dpy.write("\n")
     dpy.write("\n")
     dpy.write("class DS:\n")
 
-    dtxt = "C:/repository/esptool-master/lol/ds_ow_functions.txt"
+    dtxt = PATH.format("ds_ow_functions.txt")
     file_str = parseTextFile(dtxt, "ds", True)
     dpy.write(file_str)
     dpy.close()
